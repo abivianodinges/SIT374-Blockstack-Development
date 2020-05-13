7a72c6a6ff38c3d478df8a1040060c66721ce08a
Harry <lodgeharry@gmail.com>
2020-05-13 20:42:52 +1000

    added text area with issues in translating the text from one box to another, also having issues in saving and loading the file

--- src/Profile.js
+++ src/Profile.js
@@ -18,6 +18,9 @@ export default class Profile extends Component {
   	  	  return avatarFallbackImage;
   	  	},
   	  },
+      username:"",
+      currentText:"",
+      currentDocument:""
   	};
   }
 
@@ -26,28 +29,90 @@ export default class Profile extends Component {
     const { person } = this.state;
     return (
       !userSession.isSignInPending() ?
-      <div className="panel-welcome" id="section-2">
-        <div className="avatar-section">
-          <img src={ person.avatarUrl() ? person.avatarUrl() : avatarFallbackImage } className="img-rounded avatar" id="avatar-image" alt=""/>
-        </div>
-        <h1>Hello, <span id="heading-name">{ person.name() ? person.name() : 'Nameless Person' }</span>!</h1>
-        <p className="lead">
-          <button
-            className="btn btn-primary btn-lg"
-            id="signout-button"
-            onClick={ handleSignOut.bind(this) }
-          >
-            Logout
-          </button>
-        </p>
-      </div> : null
+
+        <div classname="mainsection">
+          <div className="panel-welcome" id="section-2">
+            <div className="avatar-section">
+              <img src={ person.avatarUrl() ? person.avatarUrl() : avatarFallbackImage } className="img-rounded avatar" id="avatar-image" alt=""/>
+            </div>
+            <p className="lead">
+              <button
+                className="btn btn-primary btn-lg"
+                id="signout-button"
+                onClick={ handleSignOut.bind(this) }>
+                Logout
+              </button>
+            </p>
+          </div>
+
+          <div id="section-3">
+            <div id="Text-section">
+              <div id="Fill-in-text">
+                <textarea className="input-status"
+                  placeholder="Document text goes here"
+                  onChange={e=>this.HandleChange(e)}
+                  />
+                </div>
+              <div id="Formatted-text">
+                <textarea className="input-status"
+                  placeholder="formatted text goes here"
+                  disabled='disabled'>
+                  this text will go</textarea>
+              </div>
+              <div id="Submit-button">
+                <button
+                onClick={e => this.handleTextSubmit(e)}
+                >
+                Submit Changes
+                </button>
+              </div>
+            </div>
+            </div>
+          </div>:null
+
+
+
+
     );
+
+
   }
 
   componentWillMount() {
-    const { userSession } = this.props;
-    this.setState({
-      person: new Person(userSession.loadUserData().profile),
-    });
+    this.fetchData()
   }
+
+  HandleChange(event){
+    this.setState({currentText:event.target.value})
+    document.getElementById('Formatted-text').value=this.state.currentText;
+  }
+  handleTextSubmit(event){
+    let newText = {
+     text: this.state.currentText,
+     created_at: Date.now()
+   }
+   const { userSession } = this.props
+   const options = { encrypt: false }
+   userSession.putFile('TextDocs.json', JSON.stringify(newText), options)
+     .then(() => {
+       document.getElementById('Formatted-text').value=this.state.currentText;
+       })
+
+  }
+
+  fetchData() {
+  const { userSession } = this.props
+  const options = { decrypt: false }
+
+  userSession.getFile('TextDocs.json', options)
+    .then((file) => {
+      var currentDocument = JSON.parse(file || '[]')
+      this.setState({
+        person: new Person(userSession.loadUserData().profile),
+        username: userSession.loadUserData().username,
+        currentText: userSession.loadUserData().text
+      })
+    })
+  }
+
 }
